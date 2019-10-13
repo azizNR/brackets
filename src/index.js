32a9accd2baace7df8048a6b30b971f5f5621bb3
unknown <aziz.narkabilov@gmail.com>
2019-10-14 00:36:16 +0500

    bracket

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+    let bracketsArray = str.split('');
+    let configArray = [].concat.apply([], bracketsConfig);
+
+    for(let i = 0; i < bracketsArray.length; i++) {
+        for(let j = 0; j < configArray.length; j+=2) {
+            if(configArray[j] == bracketsArray[i] && configArray[j+1] == bracketsArray[i+1]) {
+                bracketsArray.splice(i,2);
+                i-=2;
+            }
+        }
+    }
+    console.log(bracketsArray);
+    return (bracketsArray.length == 0);
 }
